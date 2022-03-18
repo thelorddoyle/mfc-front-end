1146099d1ea5a9fa3c3a43fff1595fd7db6ba704
Jesus-fhz <jesusflores9325@gmail.com>
2022-03-18 12:36:27 +1100

    Exporting redux provider and store

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,24 @@
+//MODULES
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { Provider } from 'react-redux'
+import { store } from './config/store'
+
+//COMPONENTS
 import Navbar from './components/Navbar';
 import FrontPage from './components/FrontPage';
+
+
 function App() {
   return (
+  <Provider store={ store }>
     <Router>
         <Navbar/>
         <Routes>
           <Route path="/" element={ <FrontPage/> } />
         </Routes>
     </Router>
+  </Provider>
   );
 }
 
