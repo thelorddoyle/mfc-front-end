db14f4b132dc5ad9deeff1c745349ae5ac0729c0
Jesus-fhz <jesusflores9325@gmail.com>
2022-03-18 17:12:06 +1100

    Using dispatch and adding graphql mutation

--- src/components/FrontPage.tsx
+++ src/components/FrontPage.tsx
@@ -1,7 +1,28 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { gql } from 'graphql-tag'
+import { useMutation } from '@apollo/client'
+import { useDispatch } from "react-redux";
 
 const FrontPage: React.FC = () => {
+    const [loginUser, {loading}] = useMutation(LOGIN_USER, {
+        update(_, {data: {login: userData}}){
+            //context.login(userData)
+            console.log(userData);
+            
+       },
+       onError(err){
+        console.log(err.graphQLErrors[0].extensions.errors);
+         
+       },
+       variables: {
+           username: "user",
+           password: "chicken"
+       }
+   })
 
+useEffect(()=>{
+    loginUser();
+},[])
     return (
         <div>
             MFC-Metaverse
@@ -9,5 +30,20 @@ const FrontPage: React.FC = () => {
     )
 }
 
+
+const LOGIN_USER = gql`
+  
+    mutation login(
+        $username: String!
+        $password: String!
+    ){
+        login(
+            username: $username
+            password: $password
+        ){
+            id email username createdAt token
+        }
+    }
+`
 
 export default FrontPage
