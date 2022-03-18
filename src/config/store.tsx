3b65a3c0a95c6fb77b203f6a4b705721902a6a79
Daniel Lord-Doyle <dlorddoyle@gmail.com>
2022-03-18 15:46:06 +1100

    added in redux thunk and began set up of redures

--- src/config/store.tsx
+++ src/config/store.tsx
@@ -1,7 +1,11 @@
-import { createStore, compose } from 'redux';
+import { createStore, compose, applyMiddleware } from 'redux';
+import thunk from 'redux-thunk';
+import gql from 'graphql-tag';
 
 const initialState: object = {
-    user: {}
+    data: {},
+    pending: false,
+    error: null
 }
 declare global {
     interface Window {
@@ -13,15 +17,20 @@ const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose
 const reducer = ( state: object = initialState, action: any  ) => {
 
     switch(action.type){
-        case 'LOGIN':
+        case PENDING_USER_FETCH:
             return {
                 ...state,
                 user: action.payload
             }
-        case 'LOGOUT':
+        case SUCCEED_USER_FETCH:
             return {
                 ...state,
                 user: {}
+            }
+        case FAIL_USER_FETCH:
+            return {
+                ...state,
+                error: action.error
             }
         default:
             return state
@@ -30,5 +39,24 @@ const reducer = ( state: object = initialState, action: any  ) => {
 
 export const store = createStore(
     reducer,
+    applyMiddleware(thunk),
     composeEnhancers()
 )
+
+const PENDING_USER_FETCH = 'user/PENDING_USER_FETCH'
+const SUCCEED_USER_FETCH = 'user/SUCCEED_USER_FETCH'
+const FAIL_USER_FETCH = 'user/FAIL_USER_FETCH'
+
+export const pendingUser = () => ({
+    type: PENDING_USER_FETCH
+})
+
+export const succeedUserFetch = (user: any) => ({
+    type: SUCCEED_USER_FETCH,
+    data: user
+})
+
+export const failUserFetch = (err: any) => ({
+    type: FAIL_USER_FETCH,
+    error: err
+})
