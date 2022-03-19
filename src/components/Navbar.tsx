56caf31bc02e27cbcdccde0eafe9462c383f7ee4
Jesus-fhz <jesusflores9325@gmail.com>
2022-03-19 15:03:36 +1100

    saving token into localstorage

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -6,7 +6,7 @@ const Navbar: React.FC = () =>{
     return (
         <>
             <Link to="/login">Login</Link>
-
+            <Link to="/">Home</Link>
         </>
     )
 }
--- src/config/store.tsx
+++ src/config/store.tsx
@@ -1,12 +1,31 @@
 import { createStore, compose, applyMiddleware } from 'redux';
 import thunk from 'redux-thunk';
+import jwtDecode from "jwt-decode";
 
 
-const initialState: object = {
+
+
+const initialState: any = {
     data: {},
     pending: false,
    
 }
+//Check if Token hasn't expired 
+if(localStorage.getItem('token')){
+    //We decode token to get expiration date
+    const decodeToken:any = jwtDecode(localStorage.getItem('token') || '{}')
+    //If expired we remove token 
+    if(Date.now() >= (decodeToken.exp * 1000)){
+        localStorage.removeItem('token')
+    }else{
+        //We set the user state with the his information
+        initialState.data = { 
+            ...initialState.data,
+            ...decodeToken
+        } ;
+    }
+}
+
 declare global {
     interface Window {
         __REDUX_DEVTOOLS_EXTENSION_COMPOSE__?: typeof compose;
@@ -17,6 +36,7 @@ const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose
 const reducer = ( state: object = initialState, action: any  ) => {
     switch(action.type){
         case "loginUser":
+            localStorage.setItem('token', action.payload.token)
             return {
                 ...state,
                 data: action.payload
