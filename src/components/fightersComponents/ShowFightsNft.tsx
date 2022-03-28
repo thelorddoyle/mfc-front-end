7a00931ff0383ba9cb8cb0ad80b2b7862236df4d
Laurence Qi <laurence.qi@outlook.com>
2022-03-28 12:57:58 +1100

    added minor comment

--- src/components/fightersComponents/ShowFightsNft.tsx
+++ src/components/fightersComponents/ShowFightsNft.tsx
@@ -9,8 +9,6 @@ interface Props{
     settingFightId: any
 }
 
-//TODO: get the nfts into an order of upcoming first then past fights. 
-
 const ShowFightsNft: React.FC<Props> = ({infoNft, settingFightId}) => {
     const [resolvedFights, setResolvedFights] = useState<any | null> ([]);
     const [upcomingFights, setUpcomingFights] = useState<any | null> ([]);
@@ -20,37 +18,38 @@ const ShowFightsNft: React.FC<Props> = ({infoNft, settingFightId}) => {
         setUpcomingFights(infoNft?.fights?.filter((fight: any) => !fight.winnerId)) // probably turn this into 1 foreach. 
     }, [infoNft]);
     
-    return(
+    //TODO: refactor this code. 
+    return( 
         <div className="past-fights">
             <h1>All Fights</h1> 
                 {
                     upcomingFights?.map(function(fight: any): JSX.Element | undefined {
                         // if the fight has been resolved
-                            return (
-                                <div className={infoNft.id === fight.winnerId ? 'fight-details fight-won' : 'fight-details fight-lost'} key={fight.id}>
-                                    <div>
-                                        <h1>Match</h1>
-                                        <span>#{truncate(fight.id)}</span>
-                                    </div>
-                                    <div>
-                                        <h1>User</h1>
-                                        <span>Upcoming</span>
-                                    </div>
-                                    <div>
-                                        <h1>Winner: </h1>
-                                        <span>#{truncate(fight.winnerId)}</span>
-                                    </div>
-                                    <div>
-                                        <h1>Loser: </h1>
-                                        <span>
-                                            #{truncate(fight.loserId)}
-                                        </span>
-                                    </div>
-                                    <div key={fight.id} onClick={()=> settingFightId(fight.id) }>
-                                        <button>View Fight</button>
-                                    </div>
+                        return (
+                            <div className={infoNft.id === fight.winnerId ? 'fight-details fight-won' : 'fight-details fight-lost'} key={fight.id}>
+                                <div>
+                                    <h1>Match</h1>
+                                    <span>#{truncate(fight.id)}</span>
                                 </div>
-                            )
+                                <div>
+                                    <h1>User</h1>
+                                    <span>Upcoming</span>
+                                </div>
+                                <div>
+                                    <h1>Winner: </h1>
+                                    <span>#{truncate(fight.winnerId)}</span>
+                                </div>
+                                <div>
+                                    <h1>Loser: </h1>
+                                    <span>
+                                        #{truncate(fight.loserId)}
+                                    </span>
+                                </div>
+                                <div key={fight.id} onClick={()=> settingFightId(fight.id) }>
+                                    <button>View Fight</button>
+                                </div>
+                            </div>
+                        )
                     })
                 }
                 {
